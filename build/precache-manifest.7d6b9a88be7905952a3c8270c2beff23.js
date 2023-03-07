b6deeeea5d0fbfdcf36a9b45859c2b05396109c9
AKHIL S S <93428847+Akhil234@users.noreply.github.com>
2023-03-07 23:02:24 +0530

    Add files via upload

---
+++ build/precache-manifest.7d6b9a88be7905952a3c8270c2beff23.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5026ae7fe55abc9e0cd4f91cdc8f8d8d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4aa8ca3e23abd5c7c46e",
+    "url": "/static/css/main.169d2978.chunk.css"
+  },
+  {
+    "revision": "7cb53b30ae861de758b7",
+    "url": "/static/js/2.e49f04a6.chunk.js"
+  },
+  {
+    "revision": "4aa8ca3e23abd5c7c46e",
+    "url": "/static/js/main.22097198.chunk.js"
+  },
+  {
+    "revision": "0a31ddf7382e0363a369",
+    "url": "/static/js/runtime-main.b240f91f.js"
+  }
+]);
